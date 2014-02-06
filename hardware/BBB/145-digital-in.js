4f509931ba9159285673b86498e9792a9e8ebdc0
Maxwell Hadley <mrh.github@susato.demon.co.uk>
2014-02-06 22:11:09 +0000

    Debugging discrete-in
    Coerce the currentState to always be numeric & use === tests
    Catch the first spurious interrupt when x.value is undefined
    Add more debug output
    Add a second timer to get the first message out. This still fails at
    node-RED startup when the node is not activeLow

--- hardware/BBB/145-digital-in.js
+++ hardware/BBB/145-digital-in.js
@@ -19,9 +19,9 @@ var RED = require(process.env.NODE_RED_HOME + "/red/red");
 
 // Require bonescript
 try {
-	var bs = require("bonescript");
-} catch(err) {
-	require("util").log("[145-digital-in] Error: cannot find module 'bonescript'");
+    var bs = require("bonescript");
+} catch (err) {
+    require("util").log("[145-digital-in] Error: cannot find module 'bonescript'");
 }
 
 // The node constructor
@@ -29,117 +29,135 @@ function DiscreteInputNode(n) {
     RED.nodes.createNode(this, n);
 
     // Store local copies of the node configuration (as defined in the .html)
-    this.topic = n.topic;							// the topic is not currently used
-    this.pin = n.pin;								// The Beaglebone Black pin identifying string
-	if (n.activeLow)								// Set the 'active' state 0 or 1 as appropriate
-		this.activeState = "0";
-	else
-		this.activeState = "1";
-	this.updateInterval = n.updateInterval*1000; 	// How often to send total active time messages
+    this.topic = n.topic;                           // the topic is not currently used
+    this.pin = n.pin;                               // The Beaglebone Black pin identifying string
+    if (n.activeLow)                                // Set the 'active' state 0 or 1 as appropriate
+        this.activeState = 0;
+    else
+        this.activeState = 1;
+    this.updateInterval = n.updateInterval * 1000;  // How often to send totalActiveTime messages
 
-	this.interruptAttached = false;	// Flag: should we detach interrupt when we are closed?
-	this.intervalId = null;			// Remember the timer ID so we can delete it when we are closed
-	this.currentState = 0;			// The pin input state "1" or "0"
-	this.lastActiveTime = 0;		// The date (in ms since epoch) when the pin last went high
-	this.totalActiveTime = 0;		// The total time in ms that the pin has been high (since reset)
-	this.starting = true;
-	
-	// Define 'node' to allow us to access 'this' from within callbacks (the 'var' is essential -
-	// otherwise there is only one 'node' for all instances of DiscreteInputNode!)
+    this.interruptAttached = false; // Flag: should we detach interrupt when we are closed?
+    this.intervalId = null;         // Remember the timer ID so we can delete it when we are closed
+    this.currentState = 0;          // The pin input state "1" or "0"
+    this.lastActiveTime = NaN;      // The date (in ms since epoch) when the pin last went high
+    this.totalActiveTime = 0;       // The total time in ms that the pin has been high (since reset)
+    this.starting = true;
+    
+    // Define 'node' to allow us to access 'this' from within callbacks (the 'var' is essential -
+    // otherwise there is only one global 'node' for all instances of DiscreteInputNode!)
     var node = this;
 
-	// This function is called whenever the input pin changes state. We update the currentState
-	// and the ActiveTime variables, and send a message on the first output with the new state
-	var interruptCallback = function (x) {
-			if (node.currentState == x.value) {
-				node.log("Spurious interrupt: " + x.value);
-			} else {
-				node.currentState = x.value;
-				var now = Date.now();
-				if (node.currentState == node.activeState) {
-					node.lastActiveTime = now;
-				} else {
-					node.totalActiveTime += now - node.lastActiveTime;
-				}
-				var msg = {};
-				msg.topic = node.topic;
-				msg.payload = node.currentState;
-				node.send([msg, null]);
-			}
-		};
+    // This function is called whenever the input pin changes state. We update the currentState
+    // and the ActiveTime variables, and send a message on the first output with the new state
+    // Note: this function gets called spuriously when the interrupt is first enabled: in this
+    // case x.value is undefined - we must test for this
+    var interruptCallback = function (x) {
+            node.log("interruptCallback: x.value = " + x.value);
+            node.log("interruptCallback: node.currentState = " + node.currentState);
+            node.log("interruptCallback: node.totalActiveTime = " + node.totalActiveTime);
+            node.log("interruptCallback: node.lastActiveTime = " + node.lastActiveTime);
+            if (node.currentState === x.value - 0) {
+                node.log("Spurious interrupt: " + x.value);
+            } else if (x.value != undefined) {
+                node.currentState = x.value - 0;
+                var now = Date.now();
+                if (node.currentState === node.activeState) {
+                    node.lastActiveTime = now;
+                } else if (!isNaN(node.lastActiveTime)) {
+                    node.totalActiveTime += now - node.lastActiveTime;
+                }
+                var msg = {};
+                msg.topic = node.topic;
+                msg.payload = node.currentState;
+                node.send([msg, null]);
+            }
+        };
 
-	// This function is called by the timer. It updates the ActiveTime variables, and sends a
-	// message on the second output with the latest value of the total active time, in seconds
-	var timerCallback = function () {
-			if (node.currentState == node.activeState) {
-				var now = Date.now();
-				node.totalActiveTime += now - node.lastActiveTime;
-				node.lastActiveTime = now;
-			}
-			var msg = {};
-			msg.topic = node.topic;
-			msg.payload = node.totalActiveTime/1000;
-			node.send([null, msg]);
-		};
+    // This function is called by the timer. It updates the ActiveTime variables, and sends a
+    // message on the second output with the latest value of the total active time, in seconds
+    var timerCallback = function () {
+            node.log("timerCallback: node.currentState = " + node.currentState);
+            node.log("timerCallback: node.totalActiveTime = " + node.totalActiveTime);
+            node.log("timerCallback: node.lastActiveTime = " + node.lastActiveTime);
+            if (node.currentState === node.activeState) {
+                var now = Date.now();
+                node.totalActiveTime += now - node.lastActiveTime;
+                node.lastActiveTime = now;
+            }
+            var msg = {};
+            msg.topic = node.topic;
+            msg.payload = node.totalActiveTime / 1000;
+            node.send([null, msg]);
+        };
 
-	// This function is called when we receive an input message. Clear the ActiveTime variables
-	// (so we start counting from zero again)
-	var inputCallback = function (msg) {
-			node.totalActiveTime = 0;
-			if (node.currentState == node.activeState) {
-				node.lastActiveTime = Date.now();
-			}
-			if (node.starting) {
-			 	node.starting = false;
-				var msg = [{topic:node.topic}, {topic:node.topic}];
-				msg[0].payload = node.currentState;
-				msg[1].payload = node.totalActiveTime;
-				this.send(msg);
-				node.log("Initial message: " + msg[0].payload + " " + msg[1].payload);
-				node.log("currentState: " + this.currentState);
-				node.log("activeTime: " + this.totalActiveTime);
-			}
-		};
+    // This function is called when we receive an input message. Clear the ActiveTime variables
+    // (so we start counting from zero again)
+    var inputCallback = function (msg) {
+            node.log("inputCallback: node.currentState = " + node.currentState);
+            node.log("inputCallback: node.totalActiveTime = " + node.totalActiveTime);
+            node.log("inputCallback: node.lastActiveTime = " + node.lastActiveTime);
+            node.totalActiveTime = 0;
+            if (node.currentState === node.activeState) {
+                node.lastActiveTime = Date.now();
+            }
+            if (node.starting) {
+                node.starting = false;
+                var msg = [{topic:node.topic}, {topic:node.topic}];
+                msg[0].payload = node.currentState;
+                msg[1].payload = node.totalActiveTime;
+                node.send(msg);
+                node.log("Initial message: " + msg[0].payload + " " + msg[1].payload);
+                node.log("currentState: " + node.currentState);
+                node.log("activeTime: " + node.totalActiveTime);
+            }
+        };
 
-	// If we have a valid pin, set it as an input and read the (digital) state
-	if (["P8_7", "P8_8", "P8_9", "P8_10", "P8_11", "P8_12", "P8_13", "P8_14", "P8_15",
-		 "P8_16", "P8_17", "P8_18", "P8_19", "P8_26", "P9_11", "P9_12", "P9_13", "P9_14",
-		 "P9_15", "P9_16", "P9_17", "P9_18", "P9_21", "P9_22", "P9_23", "P9_24", "P9_26",
-		 "P9_27", "P9_30", "P9_41", "P9_42"].indexOf(node.pin) >= 0) {
-		bs.pinMode(node.pin, bs.INPUT);
-		bs.digitalRead(node.pin, function (x) {
-				// Initialise the currentState and lastActveTime variables based on the value read
-				node.currentState = x.value;
-				node.error("First read - currentState: " + node.currentState);
-				if (node.currentState == node.activeState) {
-					node.lastActiveTime = Date.now();
-				}
-				// Attempt to attach a change-of-state interrupt handler to the pin. If we succeed,
-				// set the input event and interval handlers, then send an initial message with the
-				// pin state on the first output
-				if (bs.attachInterrupt(node.pin, true, bs.CHANGE, interruptCallback)) {
-					node.interruptAttached = true;
-					node.on("input", inputCallback);
-					node.intervalId = setInterval(timerCallback, node.updateInterval);
-				} else {
-					node.error("Failed to attach interrupt");
-				}
-				setTimeout(function () { node.emit("input", {}); }, 50);
-			});
-	} else {
-		node.error("Unconfigured input pin");
-	}
+    // If we have a valid pin, set it as an input and read the (digital) state
+    if (["P8_7", "P8_8", "P8_9", "P8_10", "P8_11", "P8_12", "P8_13", "P8_14", "P8_15",
+         "P8_16", "P8_17", "P8_18", "P8_19", "P8_26", "P9_11", "P9_12", "P9_13", "P9_14",
+         "P9_15", "P9_16", "P9_17", "P9_18", "P9_21", "P9_22", "P9_23", "P9_24", "P9_26",
+         "P9_27", "P9_30", "P9_41", "P9_42"].indexOf(node.pin) >= 0) {
+        setTimeout(function () {
+			bs.pinMode(node.pin, bs.INPUT);
+			bs.digitalRead(node.pin, function (x) {
+						// Initialise the currentState and lastActveTime variables based on the value read
+						node.log("digitalRead: x.value = " + x.value);
+						node.log("digitalRead: node.currentState = " + node.currentState);
+						node.log("digitalRead: node.totalActiveTime = " + node.totalActiveTime);
+						node.log("digitalRead: node.lastActiveTime = " + node.lastActiveTime);
+						node.currentState = x.value - 0;
+						node.log("First read - currentState: " + node.currentState);
+						if (node.currentState === node.activeState) {
+							node.lastActiveTime = Date.now();
+						}
+						// Attempt to attach a change-of-state interrupt handler to the pin. If we succeed,
+						// set the input event and interval handlers, then send an initial message with the
+						// pin state on the first output
+						if (bs.attachInterrupt(node.pin, true, bs.CHANGE, interruptCallback)) {
+							node.interruptAttached = true;
+							node.on("input", inputCallback);
+							node.intervalId = setInterval(timerCallback, node.updateInterval);
+						} else {
+							node.error("Failed to attach interrupt");
+						}
+						setTimeout(function () { node.emit("input", {}); }, 50);
+					});
+				}, 50);
+    } else {
+        node.error("Unconfigured input pin");
+    }
 }
 
 // Register the node by name. This must be called before overriding any of the Node functions.
 RED.nodes.registerType("discrete-in", DiscreteInputNode);
 
-// On close, detach the interrupt (if we attaced one) and clear the interval (if we set one)
+// On close, detach the interrupt (if we attached one) and clear the interval (if we set one)
 DiscreteInputNode.prototype.close = function () {
-	if (this.interruptAttached) {
-		bs.detachInterrupt(this.pin);
-	}
-	if (this.intervalId != null) {
-		clearInterval(this.intervalId);
-	}
+    if (this.interruptAttached) {
+        bs.detachInterrupt(this.pin);
+    }
+    if (this.intervalId !== null) {
+        clearInterval(this.intervalId);
+    }
 };
