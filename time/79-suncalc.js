fdeb0ba71c1d223e1e95a913880ba89df27d33b4
John Cooper <github@choffee.co.uk>
2014-03-16 20:44:41 +0000

    Update suncalc for new moonIllumination code.
    The Node Suncal library has changed the moon function name this changes the code to cope with it.
    
    In this commit to suncalc.
    https://github.com/mourner/suncalc/commit/bf5ee6377e8b829974046d02389d7bd05ee4d95f
    So now this causes node to crash.

--- time/79-suncalc.js
+++ time/79-suncalc.js
@@ -38,7 +38,7 @@ function SunNode(n) {
         var mins2 = times[node.end].getUTCMinutes();
         var e1 = (hour*60+mins) - (hour1*60+mins1);
         var e2 = (hour*60+mins) - (hour2*60+mins2);
-        var moon = parseInt(SunCalc.getMoonFraction(now)*100)/100;
+        var moon = SunCalc.getMoonIllumination(now).fraction;
         msg = { payload:0, topic:"sun", moon:moon };
         if ((e1 > 0) & (e2 < 0)) { msg.payload = 1; }
         if (oldval == null) { oldval = msg.payload; }
