5782a4ee85dc72c196aa57369613e927fdfc299b
Dave C-J <dave@conway-jones.co.uk>
2014-03-31 14:26:06 +0100

    Update notify node to pick up renamed node-red icon image

--- social/notify/57-notify.js
+++ social/notify/57-notify.js
@@ -16,24 +16,24 @@
 
 var RED = require(process.env.NODE_RED_HOME+"/red/red");
 var growl = require('growl');
-var imagefile = process.env.NODE_RED_HOME+"/public/mqtt-node-red.png";
+var imagefile = process.env.NODE_RED_HOME+"/public/node-red.png";
 
 function NotifyNode(n) {
-	RED.nodes.createNode(this,n);
-	this.title = n.title;
-	var node = this;
-	this.on("input",function(msg) {
-		var titl = this.title||msg.topic;
-		if (typeof(msg.payload) == 'object') {
-			msg.payload = JSON.stringify(msg.payload);
-		}
-		if (typeof(titl) != 'undefined') {
-			growl(msg.payload, { title: titl, image: imagefile });
-		}
-		else {
-			growl(msg.payload, { image: imagefile });
-		}
-	});
+    RED.nodes.createNode(this,n);
+    this.title = n.title;
+    var node = this;
+    this.on("input",function(msg) {
+        var titl = this.title || msg.topic;
+        if (typeof(msg.payload) == 'object') {
+            msg.payload = JSON.stringify(msg.payload);
+        }
+        if (typeof(titl) != 'undefined') {
+            growl(msg.payload, { title: titl, image: imagefile });
+        }
+        else {
+            growl(msg.payload, { image: imagefile });
+        }
+    });
 }
 
 RED.nodes.registerType("notify",NotifyNode);
