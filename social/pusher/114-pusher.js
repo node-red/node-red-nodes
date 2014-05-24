fe0222d49871ea948c093228104690b276054c5b
Dave C-J <dave@conway-jones.co.uk>
2014-05-24 16:38:35 +0100

    Quick tidy up of pusher node - remove final key from flow, fix in/out order on palette.

--- social/pusher/114-pusher.js
+++ social/pusher/114-pusher.js
@@ -1,75 +1,28 @@
 /**
- * pusher_send.js
- * Subscription module for the Pusher service (www.pusher.com)
- * Requires 'pusher' module 
- * Copyright 2014 Charalampos Doukas - @BuildingIoT
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- **/
-
+* pusher.js
+* Subscription module for the Pusher service (www.pusher.com)
+* Requires 'pusher' and 'pusher-client' modules.
+*
+* Copyright 2014 Charalampos Doukas, @BuildingIoT
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+**/
 
 var Pusher = require('pusher');
 var PusherClient = require('pusher-client');
 
 // Require main module
 var RED = require(process.env.NODE_RED_HOME+"/red/red");
-
-
-// The main node definition - most things happen in here
-//Node for sending Pusher events
-function PusherNodeSend(n) {
-    // Create a RED node
-    RED.nodes.createNode(this,n);
-
-    var node = this;
-
-    var credentials = RED.nodes.getCredentials(n.id);
-
-    if ((credentials) && (credentials.hasOwnProperty("pusherappid"))) { this.appid = credentials.pusherappid; }
-    else { this.error("No Pusher api token set"); }
-    if ((credentials) && (credentials.hasOwnProperty("pusherappsecret"))) { this.appsecret = credentials.pusherappsecret; }
-    else { this.error("No Pusher user secret set"); }
-
-    if ((credentials) && (credentials.hasOwnProperty("pusherappkey"))) { this.appkey = credentials.pusherappkey; }
-    else { this.error("No Pusher user key set"); }
-
-    //get parameters from user
-    this.channel = n.channel;
-    this.eventname = n.eventname;
-
-    var pusher = new Pusher({
-            appId: this.appid,
-            key: this.appkey,
-            secret: this.appsecret
-    });
-
-
-    this.on("input", function(msg){
-        
-        pusher.trigger(this.channel, this.eventname, {
-            "message": ""+msg.payload
-        });
-            
-    });
-
-
-    this.on("close", function() {
-        // Called when the node is shutdown - eg on redeploy.
-        // Allows ports to be closed, connections dropped etc.
-        // eg: this.client.disconnect();
-    });
-
- }
 
 //node for subscribing to an event/channel
  function PusherNode(n) {
@@ -86,50 +39,69 @@ function PusherNodeSend(n) {
     this.channel = n.channel;
     this.eventname = n.eventname;
 
-
-    
     //create a subscription to the channel and event defined by user
     var socket = new PusherClient(''+this.appkey);
     var my_channel = socket.subscribe(''+this.channel);
     socket.bind(''+this.eventname,
         function(data) {
-    
             var msg = {};
             msg.payload = data;
             node.send(msg);
         }
     );
 
-    this.on("input", function(msg){
-            
-    });
-
-
     this.on("close", function() {
-        // Called when the node is shutdown - eg on redeploy.
-        // Allows ports to be closed, connections dropped etc.
-        // eg: this.client.disconnect();
         socket.disconnect();
     });
+}
 
+//Node for sending Pusher events
+function PusherNodeSend(n) {
+    // Create a RED node
+    RED.nodes.createNode(this,n);
+
+    var node = this;
+
+    var credentials = RED.nodes.getCredentials(n.id);
+
+    if ((credentials) && (credentials.hasOwnProperty("pusherappid"))) { this.appid = credentials.pusherappid; }
+    else { this.error("No Pusher api token set"); }
+    if ((credentials) && (credentials.hasOwnProperty("pusherappsecret"))) { this.appsecret = credentials.pusherappsecret; }
+    else { this.error("No Pusher user secret set"); }
+    if ((credentials) && (credentials.hasOwnProperty("pusherappkey"))) { this.appkey = credentials.pusherappkey; }
+    else { this.error("No Pusher user key set"); }
+
+    //get parameters from user
+    this.channel = n.channel;
+    this.eventname = n.eventname;
+
+    var pusher = new Pusher({
+            appId: this.appid,
+            key: this.appkey,
+            secret: this.appsecret
+    });
+
+    this.on("input", function(msg){
+        pusher.trigger(this.channel, this.eventname, {
+            "message": ""+msg.payload
+        });
+    });
+
+    this.on("close", function() {
+    });
  }
 
  //debugging on the output:
- var displayResult = function(result) {
-    console.log(result);
+var displayResult = function(result) {
+    node.log(result);
 };
 
 var displayError = function(err) {
-    console.error(err);
+    node.log("Error: "+err);
 };
 
-
-
-
-// Register the node by name. This must be called before overriding any of the
-// Node functions.
-RED.nodes.registerType("pusher out",PusherNodeSend);
 RED.nodes.registerType("pusher in",PusherNode);
+RED.nodes.registerType("pusher out",PusherNodeSend);
 
 var querystring = require('querystring');
 
@@ -155,7 +127,7 @@ RED.httpAdmin.post('/pusher/:id',function(req,res) {
     req.on('end', function(){
         var newCreds = querystring.parse(body);
         var credentials = RED.nodes.getCredentials(req.params.id)||{};
-        
+
         if (newCreds.pusherappid == null || newCreds.pusherappid == "") {
             delete credentials.pusherappid;
         } else {
@@ -179,9 +151,7 @@ RED.httpAdmin.post('/pusher/:id',function(req,res) {
             credentials.pusherappkey_sub = newCreds.pusherappkey_sub||credentials.pusherappkey_sub;
         }
 
-
         RED.nodes.addCredentials(req.params.id,credentials);
         res.send(200);
     });
 });
-
