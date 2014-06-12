5762eaec1a805885ceb92dce5f69a230327b4a34
Nicholas Humfrey <njh@aelius.com>
2014-06-12 15:34:24 +0100

    Added OSC input node

--- io/osc/36-osc.js
+++ io/osc/36-osc.js
@@ -18,6 +18,42 @@ module.exports = function(RED) {
     "use strict";
     var osc = require('node-osc');
 
+    // The Input Node
+    function OSCin(n) {
+        RED.nodes.createNode(this,n);
+        this.addr = n.addr;
+        this.port = n.port;
+        var node = this;
+
+        var server = new osc.Server(this.port, this.addr);
+        server.on('message', function (message, remote) {
+            var topic = message.shift();
+            if (message.length == 0) {
+                message = "";
+            } else if (message.length == 1) {
+                message = message[0];
+            }
+            var msg = { topic:topic, payload:message, ip:remote.address, port:remote.port };
+            node.send(msg);
+        });
+
+        server.on('listening', function () {
+            var address = server.address();
+            node.log('OSC listener at ' + address.address + ":" + address.port);
+        });
+
+        node.on("close", function() {
+            try {
+                server.kill();
+                node.log('OSC listener stopped');
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType("osc in", OSCin);
+
+
     // The Output Node
     function OSCout(n) {
         RED.nodes.createNode(this,n);
