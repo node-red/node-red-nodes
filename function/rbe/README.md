9c36a44545b804981e19b0397ed8e24c07eb6e7c
dceejay <conway@uk.ibm.com>
2015-01-23 13:34:10 +0000

    Add simple RBE node

---
+++ function/rbe/README.md
@@ -0,0 +1,28 @@
+node-red-node-rbe
+=================
+
+A <a href="http://nodered.org" target="_new">Node-RED</a> node that provides provides report-by-exception (RBE) and deadband capability.
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+    npm install node-red-node-rbe
+
+
+Usage
+-----
+
+A simple node to provide report by exception (RBE) and deadband function - only passes on data if it has changed.
+
+###RBE mode
+
+Outputs the **msg**  if the **msg.payload** is different to the previous one. Works on numbers and strings. Useful for filtering out repeated messages of the same value. Saves bandwidth, etc...
+
+###Deadband mode
+
+In deadband mode the incoming payload should contain a parseable *number* and is
+    output only if greater than + or - the *band gap* away from the previous output.
+
+Will accept numbers, or strings like  "18.4 C"  or "$500"
---
+++ function/rbe/rbe.js
@@ -0,0 +1,50 @@
+/**
+ * Copyright 2014 IBM Corp.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ **/
+
+module.exports = function(RED) {
+    "use strict";
+
+    function RbeNode(n) {
+        RED.nodes.createNode(this,n);
+        this.func = n.func || "rbe";
+        this.gap = n.gap || 0;
+        var node = this;
+
+        var previous = null;
+        this.on("input",function(msg) {
+            if (this.func === "rbe") {
+                if (msg.payload != previous) {
+                    previous = msg.payload;
+                    node.send(msg);
+                }
+            }
+            else {
+                var n = parseFloat(msg.payload);
+                if (previous == null) { previous = n - node.gap; }
+                if (!isNaN(n)) {
+                    if (Math.abs(n - previous) >= node.gap) {
+                        previous = n;
+                        node.send(msg);
+                    }
+                }
+                else {
+                    node.warn("no number found in payload");
+                }
+            }
+        });
+    }
+    RED.nodes.registerType("rbe",RbeNode);
+}
