4fe2f82f2fa0bbe937f268d063a62d37c4803035
Nick O'Leary <nick.oleary@gmail.com>
2015-02-13 10:32:43 +0000

    Add mqlight license/package

--- io/mqlight/mqlight.js
+++ io/mqlight/mqlight.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2013,2014 IBM Corp.
+ * Copyright 2013, 2015 IBM Corp.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -113,7 +113,8 @@ module.exports = function(RED) {
 
                 sendClient.on("started", function () {
                     node.on("input", function(msg) {
-                        if (node.topic === "") {
+                        var topic = node.topic;
+                        if (topic === "") {
                             if (msg.topic) {
                                 node.topic = msg.topic;
                             } else {
