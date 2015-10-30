bb43dd31a01c738bb9e43d5934e3d227cf389ea7
Darrell Taylor <git@darrelltaylor.com>
2015-10-30 18:47:13 +0000

    username and password for irc-out as well

--- social/irc/91-irc.js
+++ social/irc/91-irc.js
@@ -200,7 +200,7 @@ module.exports = function(RED) {
         if (node.serverConfig.ircclient === null) {
             node.log(RED._("irc.errors.connect")+": "+node.serverConfig.server);
             node.status({fill:"grey",shape:"dot",text:"node-red:common.status.connecting"});
-            var options = {autoConnect:true,autoRejoin:false,floodProtection:true,secure:node.serverConfig.ssl,selfSigned:node.serverConfig.cert,port:node.serverConfig.port,retryDelay:20000,userName:node.username,password:node.password};
+            var options = {autoConnect:true,autoRejoin:false,floodProtection:true,secure:node.serverConfig.ssl,selfSigned:node.serverConfig.cert,port:node.serverConfig.port,retryDelay:20000,userName:node.serverConfig.username,password:node.serverConfig.password};
             node.serverConfig.ircclient = new irc.Client(node.serverConfig.server, node.serverConfig.nickname, options);
             node.serverConfig.ircclient.setMaxListeners(0);
             node.serverConfig.ircclient.addListener('error', function(message) {
