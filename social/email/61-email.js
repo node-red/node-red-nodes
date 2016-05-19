b23cf0b78821f412eab34b58069ce1234044fd07
Dave Conway-Jones <conway@uk.ibm.com>
2016-05-19 10:13:47 +0100

    add envelope to email node options
    as part of node-red#875

--- social/email/61-email.js
+++ social/email/61-email.js
@@ -89,6 +89,7 @@ module.exports = function(RED) {
                     var sendopts = { from: node.userid };   // sender address
                     sendopts.to = node.name || msg.to; // comma separated list of addressees
                     sendopts.subject = msg.topic || msg.title || "Message from Node-RED"; // subject line
+                    if (msg.hasOwnProperty("envelope")) { sendopts.envelope = msg.envelope; }
                     if (Buffer.isBuffer(msg.payload)) { // if it's a buffer in the payload then auto create an attachment instead
                         if (!msg.filename) {
                             var fe = "bin";
