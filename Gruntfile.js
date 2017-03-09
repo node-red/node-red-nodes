a63df3c41d12398daf1c76b5af97deb92e433840
Dave Conway-Jones <conway@uk.ibm.com>
2017-03-09 23:18:22 +0000

    tidy up some linting files

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,20 +14,7 @@ module.exports = function(grunt) {
         },
         jshint: {
             options: {
-                jshintrc:".jshintrc", // Use external file - configured as below...
-                // http://www.jshint.com/docs/options/
-                //"asi": true,        // allow missing semicolons
-                //"curly": true,      // require braces
-                //"eqnull": true,     // ignore ==null
-                //"forin": true,      // require property filtering in "for in" loops
-                //"immed": true,      // require immediate functions to be wrapped in ( )
-                //"nonbsp": true,     // warn on unexpected whitespace breaking chars
-                ////"strict": true,   // commented out for now as it causes 100s of warnings, but want to get there eventually
-                //"loopfunc": true,   // allow functions to be defined in loops
-                //"sub": true,        // don't warn that foo['bar'] should be written as foo.bar
-                ////"unused": true,   // Check for unused functions
-                ////"forin":false,    // turn off check for "for (x in y...)"
-                //"reporter": require('jshint-stylish')
+                jshintrc:".jshintrc",   // Use external file
             },
             all: {
                 src: ['*/*.js','*/*/*.js'],
@@ -53,6 +40,7 @@ module.exports = function(grunt) {
             options: {
                 config: ".jscsrc",
                 reporter: "inline"
+                //,fix: true
             }
         }
     });
