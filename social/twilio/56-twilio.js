7af48ce10ba69a780e1ce268210ccfe3d3acc678
Dave Conway-Jones <conway@uk.ibm.com>
2017-06-05 17:26:05 +0100

    Tidy up  / hide non-supported ways of doing twill crews

--- social/twilio/56-twilio.js
+++ social/twilio/56-twilio.js
@@ -19,14 +19,10 @@ module.exports = function(RED) {
         this.from = n.from;
         this.name = n.name;
         var credentials = this.credentials;
-        if (credentials) {
-            this.token = credentials.token;
-        }
+        if (credentials) { this.token = credentials.token; }
     }
     RED.nodes.registerType("twilio-api",TwilioAPINode,{
-        credentials: {
-            token: "password"
-        }
+        credentials: { token: "password" }
     });
 
 
@@ -78,7 +74,6 @@ module.exports = function(RED) {
                         //console.log(response);
                     });
                 }
-
             }
             catch (err) {
                 node.error(err,msg);
