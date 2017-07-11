626196b33fba4f3407cc6b0a9f35b9559121df90
jonferreira <my.mail.jonatas@gmail.com>
2017-07-11 11:36:19 +0100

    Update 57-nma.js
    adds mustache templates support, all fields available on NMA API and proper handling of credentials

--- social/nma/57-nma.js
+++ social/nma/57-nma.js
@@ -1,45 +1,67 @@
+(function() {
+  module.exports = function(RED) {
+    var NMACredentialsNode, NMANotificationNode, mustache, nma;
+    mustache = require("mustache");
+	nma = require('nma');
+    NMACredentialsNode = function(config) {
+      var node;
+      RED.nodes.createNode(this, config);
+      node = this;
+      return this.pushkey = config.pushkey;
+    };
+    NMANotificationNode = function(config) {
+      var key, node, value;
+      RED.nodes.createNode(this, config);
+      node = this;
+      for (key in config) {
+        value = config[key];
+        node[key] = value;
+      }
+      this.broker = config.broker;
+      this.credentials = RED.nodes.getNode(this.broker);
+      this.pushkey = this.credentials.pushkey;
+	  
+      return this.on('input', (function(_this) {
+        return function(msg) {
+          var body, req, request;
+          node.status({
+            fill: "grey",
+            shape: "dot",
+            text: "processing"
+          });
+		  
+		var pri = msg.priority || node.priority || 0;
+		if (pri > 2) pri = 2;
+		if (pri < -2) pri = -2;
+		  
+		  nma({
+				"apikey": node.pushkey,
+				"application": mustache.render(node.application, msg) || msg.topic || "",
+				"event": mustache.render(node.event, msg) || msg.event || "",
+				"description": mustache.render(node.description, msg) || msg.payload || "",
+				"priority": pri,
+				"url": msg.url || node.url,
+				"content-type": msg.content_type || node.content_type
+			}, function (error) {
+				if (error) {
+					return node.status({
+					  shape: "dot",
+					  fill: "red",
+					  text: "error!"
+					});
+					node.error("NMA error: " + error,msg);
+				} else {
+					return node.status({});
+				}
+			});
 
-module.exports = function(RED) {
-    "use strict";
-    var nma = require('nma');
+          return node.send(msg);
+        };
+      })(this));
+	  
+    };
+    RED.nodes.registerType("NMA-credentials", NMACredentialsNode);
+    return RED.nodes.registerType("NMA-notification", NMANotificationNode);
+  };
 
-    function NMANode(n) {
-        RED.nodes.createNode(this,n);
-        this.title = n.title;
-        var credentials = this.credentials;
-        if ((credentials) && (credentials.hasOwnProperty("pushkey"))) { this.pushkey = credentials.pushkey; }
-        else { this.error("No NMA API key set"); }
-        var node = this;
-        this.on("input",function(msg) {
-            var titl = this.title||msg.topic||"Node-RED";
-            if (typeof(msg.payload) === 'object') {
-                msg.payload = JSON.stringify(msg.payload);
-            }
-            else {
-                msg.payload = msg.payload.toString();
-            }
-            if (node.pushkey) {
-                nma({
-                    "apikey": node.pushkey,
-                    "application": "Node-RED",
-                    "event": titl,
-                    "description": msg.payload,
-                    "priority": 0
-                }, function (error) {
-                    if (error) {
-                        node.error("NMA error: " + error,msg);
-                    }
-                });
-            }
-            else {
-                node.warn("NMA credentials not set.");
-            }
-        });
-    }
-
-    RED.nodes.registerType("nma",NMANode, {
-        credentials: {
-            pushkey: {type: "password"}
-        }
-    });
-}
+}).call(this);
