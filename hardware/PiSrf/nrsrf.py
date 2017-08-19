9782ea26ccfc9fbcb7b67f635e7f96319a23c9fa
denisfrench <denis.french@gmail.com>
2017-08-19 19:25:58 +1000

    Print error on exception and tweak GPIO setup

--- hardware/PiSrf/nrsrf.py
+++ hardware/PiSrf/nrsrf.py
@@ -29,14 +29,14 @@ def Measure():
         start = time.time()
         Dif = time.time() - realstart
         if Dif > 0.2:
-            print("Ultrasonic Sensor Timed out, Restarting.")
+            print("Ultrasonic Sensor Timed out (pre-echo), Restarting.")
             time.sleep(0.4)
             Main()
     while GPIO.input(ECHO)==1:
         stop = time.time()
         Dif = time.time() - realstart
         if Dif > 0.4:
-            print("Ultrasonic Sensor Timed out, Restarting.")
+            print("Ultrasonic Sensor Timed out (post-echo), Restarting.")
             time.sleep(0.2)
             Main()
 
@@ -60,8 +60,10 @@ if len(sys.argv) > 1:
 
     GPIO.setmode(GPIO.BOARD)        # Use GPIO BOARD numbers
     GPIO.setup(TRIGGER, GPIO.OUT)   # Trigger
+    GPIO.output(TRIGGER, False)     # DF
     GPIO.setup(ECHO, GPIO.OUT)      # Echo
     GPIO.output(ECHO, False)
+    time.sleep(0.1)		    # DF
     GPIO.setup(ECHO,GPIO.IN)
 
     # Flush stdin so we start clean
@@ -75,8 +77,9 @@ if len(sys.argv) > 1:
                 print(distance)
                 OLD = distance
             time.sleep(SLEEP)
-        except:                     # try to clean up on exit
-            print("0.0");
+        except Exception as e:		# try to clean up on exit
+            # print("0.0");
+            print(e)			# DF
             GPIO.cleanup(TRIGGER)
             GPIO.cleanup(ECHO)
             sys.exit(0)
