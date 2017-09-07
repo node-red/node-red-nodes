8b53d09ad329df2f78e95514db4cc135600232f6
Dave Conway-Jones <conway@uk.ibm.com>
2017-09-07 10:28:52 +0100

    handle multi-line base64 strings

--- parsers/base64/70-base64.js
+++ parsers/base64/70-base64.js
@@ -14,9 +14,10 @@ module.exports = function(RED) {
                 }
                 else if (typeof msg.payload === "string") {
                     // Take base64 string and make into binary buffer
-                    var regexp = new RegExp('^[A-Za-z0-9+\/=]*$');
-                    if ( regexp.test(msg.payload) && (msg.payload.length % 4 === 0) ) {
-                        msg.payload = new Buffer(msg.payload,'base64');
+                    var load = msg.payload.replace(/\s+/g,'');      // remove any whitespace
+                    var regexp = new RegExp('^[A-Za-z0-9+\/=]*$');  // check it only contains valid characters
+                    if ( regexp.test(load) && (load.length % 4 === 0) ) {
+                        msg.payload = new Buffer(load,'base64');
                         node.send(msg);
                     }
                     else {
