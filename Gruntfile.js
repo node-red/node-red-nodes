5b6b0c132aff0f1f28e2f8f6b55ff5646a99a534
Dave Conway-Jones <conway@uk.ibm.com>
2017-11-06 22:53:40 +0000

    clean up Gruntfile to be more consistent

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,10 +17,20 @@ module.exports = function(grunt) {
         },
         jshint: {
             options: {
-                jshintrc:".jshintrc",   // Use external file
+                jshintrc:true     // Use external jshinrc file configured as below
+                // http://www.jshint.com/docs/options/
+                //"asi": true,      // allow missing semicolons
+                //"curly": true,    // require braces
+                //"eqnull": true,   // ignore ==null
+                //"forin": true,    // require property filtering in "for in" loops
+                //"immed": true,    // require immediate functions to be wrapped in ( )
+                //"nonbsp": true,   // warn on unexpected whitespace breaking chars
+                ////"strict": true, // commented out for now as it causes 100s of warnings, but want to get there eventually
+                //"loopfunc": true, // allow functions to be defined in loops
+                //"sub": true       // don't warn that foo['bar'] should be written as foo.bar
             },
             all: {
-                src: ['*/*.js','*/*/*.js'],
+                src: ['*/*/*.js'],
                 filter: function(filepath) { // on some developer machines the test coverage HTML report utilities cause further failures
                     if ((filepath.indexOf("coverage/") !== -1) || (filepath.indexOf("node_modules") !== -1)) {
                         console.log( "\033[30m  filtered out \033[32m:\033[37m " + filepath + "\033[0m");
