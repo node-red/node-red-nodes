72cbb52fe61cb9f6dbfed02da1969a60132aadeb
borpin <brian.orpin@gmail.com>
2018-01-01 22:21:18 +0000

    Updates to match API changes
    Update to the node to match the server API changes. An attempt has been made to leave a legacy path for existing installations that may have not updated the server side.

--- io/emoncms/88-emoncms.js
+++ io/emoncms/88-emoncms.js
@@ -22,42 +22,86 @@ module.exports = function(RED) {
         this.apikey = sc.credentials.apikey;
 
         this.nodegroup = n.nodegroup || "";
+        this.datatype = n.datatype || "";
         var node = this;
         var http;
         if (this.baseurl.substring(0,5) === "https") { http = require("https"); }
         else { http = require("http"); }
         this.on("input", function(msg) {
-            this.url = this.baseurl + '/input/post.json?';
-            if (typeof(msg.payload) !== "string") {
-                this.url += 'json=' + JSON.stringify(msg.payload);
-            }
-            else {
-                if (msg.payload.indexOf(':') > -1) {
-                    this.url += 'json={' + msg.payload + '}';
+            // setup the data for the URI
+            if (this.datatype == "legacy"){
+                this.url = this.baseurl + '/input/post.json?';
+                if (typeof(msg.payload) !== "string") {
+                    this.url += 'json=' + JSON.stringify(msg.payload);
                 }
                 else {
-                    this.url += 'csv=' + msg.payload;
+                    if (msg.payload.indexOf(':') > -1) {
+                        this.url += 'json={' + msg.payload + '}';
+                    }
+                    else {
+                        this.url += 'csv=' + msg.payload;
+                    }
                 }
             }
-            this.url += '&apikey='+this.apikey;
+            else if (this.datatype == "fulljson"){
+                this.url = this.baseurl + '/input/post?';
+                this.url += 'fulljson=' + encodeURIComponent(JSON.stringify(msg.payload));
+            }
+            else if (this.datatype == "json"){
+                this.url = this.baseurl + '/input/post?';
+                this.url += 'json={' + encodeURIComponent(msg.payload) + '}';
+            }
+            else if (this.datatype == "CSV"){
+                this.url = this.baseurl + '/input/post?';
+                this.url += 'csv=' + msg.payload;
+            }
+            else {
+                node.send("ERROR : No valid data type set - " + this.datatype);
+            }
+
+            // setup the API key for URI.
+            this.url += '&apikey=' + this.apikey;
+
+            // setup the node group for URI
             var nodegroup = this.nodegroup || msg.nodegroup;
             if (nodegroup !== "") {
                 this.url += '&node=' + nodegroup;
             }
+
+            // check for a time object and setup URI if valid
             if (typeof msg.time !== 'undefined') {
-                if (!isNaN(parseInt(msg.time))) { this.url += '&time=' + msg.time; }
+                if (!isNaN(msg.time)) { 
+                    this.url += '&time=' + msg.time;
+                }
+                else {
+                    if (isNaN(Date.parse(msg.time))) {
+                        // error condition
+                        node.send("ERROR: Time object not valid - " + msg.time);
+                    } else {
+                        this.url += '&time=' + Date.parse(msg.time)/1000;
+                    }
+                }
+                delete msg.time; // clean it up for the error msg
             }
+            // debug info - uncomment if needed
+            // node.send("INFO: URI sent (decoded) - " + decodeURIComponent(this.url));
+
             http.get(this.url, function(res) {
+                msg.topic = "http response";
                 msg.rc = res.statusCode;
                 msg.payload = "";
                 res.setEncoding('utf8');
                 res.on('data', function(chunk) {
                     msg.payload += chunk;
                 });
-                res.on('end', function() {
-                    if (msg.rc === 200) {
+                res.on('end', function() { //always send response from node as a 200 does not mean data input sucess
+                        try {
+                            msg.payload = JSON.parse(msg.payload);
+                        }
+                        catch(err) {
+                            // Failed to parse, pass it on
+                        }
                         node.send(msg);
-                    }
                 });
             }).on('error', function(e) {
                 node.error(e,msg);
