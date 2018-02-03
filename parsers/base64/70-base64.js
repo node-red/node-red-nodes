f9ac210a41b6bbdfce4779409f1d6f366d9ea14b
Dave Conway-Jones <conway@uk.ibm.com>
2018-02-03 15:08:05 +0000

    Add fixed conversion modes to base64 node
    to close #401

--- parsers/base64/70-base64.js
+++ parsers/base64/70-base64.js
@@ -3,35 +3,51 @@ module.exports = function(RED) {
     "use strict";
     function Base64Node(n) {
         RED.nodes.createNode(this,n);
-        this.property = n.property||"payload";
+        this.action = n.action || "";
+        this.property = n.property || "payload";
         var node = this;
         this.on("input", function(msg) {
             var value = RED.util.getMessageProperty(msg,node.property);
             if (value !== undefined) {
-                if (Buffer.isBuffer(value)) {
-                    // Take binary buffer and make into a base64 string
-                    value = value.toString('base64');
+                if (node.action === "") {
+                    if (Buffer.isBuffer(value)) {
+                        // Take binary buffer and make into a base64 string
+                        value = value.toString('base64');
+                        RED.util.setMessageProperty(msg,node.property,value);
+                        node.send(msg);
+                    }
+                    else if (typeof value === "string") {
+                        // Take base64 string and make into binary buffer
+                        var load = value.replace(/\s+/g,'');      // remove any whitespace
+                        //var load = value.replace(/[\t\r\n\f]+/g,'');
+                        //var load = value;
+                        var regexp = new RegExp('^[A-Za-z0-9+\/=]*$');  // check it only contains valid characters
+                        if ( regexp.test(load) && (load.length % 4 === 0) ) {
+                            value = Buffer.from(load,'base64').toString('binary');
+                            RED.util.setMessageProperty(msg,node.property,value);
+                            node.send(msg);
+                        }
+                        else {
+                            node.log("Not a Base64 string - maybe we should encode it...");
+                            value = Buffer.from(value).toString('base64');
+                            RED.util.setMessageProperty(msg,node.property,value);
+                            node.send(msg);
+                        }
+                    }
+                    else {
+                        node.warn("This node only handles strings or buffers.");
+                    }
+                }
+                if (node.action === "str") {
+                    value = Buffer.from(value).toString('base64');
                     RED.util.setMessageProperty(msg,node.property,value);
                     node.send(msg);
                 }
-                else if (typeof value === "string") {
-                    // Take base64 string and make into binary buffer
-                    var load = value.replace(/\s+/g,'');      // remove any whitespace
-                    var regexp = new RegExp('^[A-Za-z0-9+\/=]*$');  // check it only contains valid characters
-                    if ( regexp.test(load) && (load.length % 4 === 0) ) {
-                        value = new Buffer(load,'base64');
-                        RED.util.setMessageProperty(msg,node.property,value);
-                        node.send(msg);
-                    }
-                    else {
-                        //node.log("Not a Base64 string - maybe we should encode it...");
-                        value = (new Buffer(value,"binary")).toString('base64');
-                        RED.util.setMessageProperty(msg,node.property,value);
-                        node.send(msg);
-                    }
-                }
-                else {
-                    node.warn("This node only handles strings or buffers.");
+                if (node.action === "b64") {
+                    var load = value.replace(/\s+/g,'');
+                    value = Buffer.from(load,'base64').toString('binary');
+                    RED.util.setMessageProperty(msg,node.property,value);
+                    node.send(msg);
                 }
             }
             else { node.warn("No property found to process"); }
