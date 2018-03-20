34199142ddd5a8760fd72dd7417abdfdcfa67a57
Dave Conway-Jones <conway@uk.ibm.com>
2018-03-20 17:17:14 +0000

    Fix piLCD line 3 and 4 addressing
    ti address #416

--- hardware/PiLcd/nrlcd.py
+++ hardware/PiLcd/nrlcd.py
@@ -31,8 +31,8 @@ LCD_CMD = False
 
 LCD_LINE_1 = 0x80 # LCD RAM address for the 1st line
 LCD_LINE_2 = 0xC0 # LCD RAM address for the 2nd line
-LCD_LINE_3 = 0xA0 # LCD RAM address for the 3rd line
-LCD_LINE_4 = 0xE0 # LCD RAM address for the 4th line
+LCD_LINE_3 = 0x94 # LCD RAM address for the 3rd line
+LCD_LINE_4 = 0xD4 # LCD RAM address for the 4th line
 
 # Timing constants
 E_PULSE = 0.0005
