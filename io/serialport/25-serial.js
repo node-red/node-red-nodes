11c75f87fcafb87a87a2ca583891291f917fdafe
Gerlando Falauto <gerlando.falauto@st.com>
2018-06-18 00:16:12 +0200

    serial: add serial request node
    Add a "serial request" node to handle simple request/response
    protocols. This node allows for multiple instances, all
    sharing the same underlying device.
    Responses coming from the serial line will only be propagated
    to the output of the node where the request was originally received
    (contrary to the "serial in" nodes which all emit the data
    received from the serial line).
    
    Every request received as an input to the node, is transmitted
    to the serial line, and a matching response must be received
    before the next one can be transmitted.
    Any input message received in the meantime is internally enqueued.
    
    The node is essentially a merge of serial in and serial out.
    It shares the same configuration with serial in and serial out
    for any given port and will not affect the behavior of the
    existing nodes.
    This means you can use, alongside with the request node:
    - as many serial in nodes as you want -- e.g. to "sniff"
    - serial out to inject mailicious/tampering data onto the serial
      line, skipping the queueing mechanism

--- io/serialport/25-serial.js
+++ io/serialport/25-serial.js
@@ -79,8 +79,8 @@ module.exports = function(RED) {
             node.status({fill:"grey",shape:"dot",text:"node-red:common.status.not-connected"});
             node.port = serialPool.get(this.serialConfig);
 
-            this.port.on('data', function(payload, port) {
-                node.send({payload:payload, port:port});
+            this.port.on('data', function(msgout) {
+                node.send(msgout);
             });
             this.port.on('ready', function() {
                 node.status({fill:"green",shape:"dot",text:"node-red:common.status.connected"});
@@ -104,6 +104,63 @@ module.exports = function(RED) {
     }
     RED.nodes.registerType("serial in",SerialInNode);
 
+
+    /******* REQUEST *********/
+    function SerialRequestNode(n) {
+        RED.nodes.createNode(this,n);
+        this.serial = n.serial;
+        this.serialConfig = RED.nodes.getNode(this.serial);
+
+        if (this.serialConfig) {
+            var node = this;
+            node.tout = null;
+            node.port = serialPool.get(this.serialConfig);
+            // Serial Out
+            node.on("input",function(msg) {
+                if (!msg.hasOwnProperty("payload")) { return; } // do nothing unless we have a payload
+                node.port.enqueue(msg,node,function(err,res) {
+                    if (err) {
+                        var errmsg = err.toString().replace("Serialport","Serialport "+node.port.serial.path);
+                        node.error(errmsg,msg);
+                    }
+                });
+            });
+
+            // Serial In
+            this.port.on('data', function(msgout, sender) {
+                // serial request will only process incoming data pertaining to its own request (i.e. when it's at the head of the queue)
+                if (sender !== node) { return; }
+                msgout.status = "OK";
+                node.send(msgout);
+            });
+            this.port.on('timeout', function(msgout, sender) {
+                if (sender !== node) { return; }
+                msgout.status = "ERR_TIMEOUT";
+                node.send(msgout);
+            });
+
+            // Common part
+            node.port.on('ready', function() {
+                node.status({fill:"green",shape:"dot",text:"node-red:common.status.connected"});
+            });
+            node.port.on('closed', function() {
+                node.status({fill:"red",shape:"ring",text:"node-red:common.status.not-connected"});
+            });
+        }
+        else {
+            this.error(RED._("serial.errors.missing-conf"));
+        }
+
+        this.on("close", function(done) {
+            if (this.serialConfig) {
+                serialPool.close(this.serialConfig.serialport,done);
+            }
+            else {
+                done();
+            }
+        });
+    }
+    RED.nodes.registerType("serial request", SerialRequestNode);
 
     var serialPool = (function() {
         var connections = {};
@@ -149,6 +206,7 @@ module.exports = function(RED) {
                         serial: null,
                         _closing: false,
                         tout: null,
+                        queue: [],
                         on: function(a,b) { this._emitter.on(a,b); },
                         close: function(cb) { this.serial.close(cb); },
                         encodePayload: function (payload) {
@@ -167,6 +225,57 @@ module.exports = function(RED) {
                             return payload;
                         },
                         write: function(m,cb) { this.serial.write(m,cb); },
+                        enqueue: function(msg,sender,cb) {
+                            var payload = this.encodePayload(msg.payload);
+                            var qobj = {
+                                sender: sender,
+                                msg: msg,
+                                payload: payload,
+                                cb: cb,
+                            }
+                            this.queue.push(qobj);
+                            if (this.queue.length === 1) {
+                                this.writehead();
+                            }
+                        },
+                        writehead: function() {
+                            if (!this.queue.length) { return; }
+                            var qobj = this.queue[0];
+                            this.write(qobj.payload,qobj.cb);
+                            var msg = qobj.msg;
+                            var timeout = msg.timeout || 10000;
+                            this.tout = setTimeout(function () {
+                                this.tout = null;
+                                var msgout = obj.dequeue() || {};
+                                msgout.port = port;
+                                // if we have some leftover stuff, just send it
+                                if (i !== 0) {
+                                    var m = buf.slice(0,i);
+                                    m = Buffer.from(m);
+                                    i = 0;
+                                    if (binoutput !== "bin") { m = m.toString(); }
+                                    msgout.payload = m;
+                                }
+                                /* Notify the sender that a timeout occurred */
+                                obj._emitter.emit('timeout',msgout,qobj.sender);
+                            }, timeout);
+                        },
+                        dequeue: function() {
+                            if (!this.queue.length) { return null; }
+                            var msg = Object.assign({}, this.queue[0].msg);
+                            msg = Object.assign(msg, {
+                                request_payload: msg.payload,
+                                request_msgid: msg._msgid,
+                            });
+                            delete msg.payload;
+                            if (this.tout) {
+                                clearTimeout(obj.tout);
+                                obj.tout = null;
+                            }
+                            this.queue.shift();
+                            this.writehead();
+                            return msg;
+                        },
                     }
                     //newline = newline.replace("\\n","\n").replace("\\r","\r");
                     var olderr = "";
@@ -216,10 +325,15 @@ module.exports = function(RED) {
                         obj.serial.on('data',function(d) {
                             function emitData(data) {
                                 var m = Buffer.from(data);
+                                var last_sender = null;
+                                if (obj.queue.length) { last_sender = obj.queue[0].sender; }
                                 if (binoutput !== "bin") { m = m.toString(); }
+                                var msgout = obj.dequeue() || {};
+                                msgout.payload = m;
+                                msgout.port = port;
                                 obj._emitter.emit('data',
-                                    /* payload */ m,
-                                    /* port */ port);
+                                    msgout,
+                                    last_sender);
                             }
 
                             for (var z=0; z<d.length; z++) {
