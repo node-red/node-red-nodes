95790e7bebeddb64e4d2ba959914a8e656415298
mattbrailsford <me@mattbrailsford.com>
2018-09-02 18:25:55 +0100

    Maintining coding standards

--- hardware/motephat/mote.js
+++ hardware/motephat/mote.js
@@ -1,129 +1,127 @@
 
 module.exports = function (RED) {
-  "use strict";
-  var fs = require('fs');
-  var spawn = require('child_process').spawn;
-  var execSync = require('child_process').execSync;
+    "use strict";
+    var fs = require('fs');
+    var spawn = require('child_process').spawn;
+    var execSync = require('child_process').execSync;
 
-  var pythonScript = __dirname + "/mote.py";
-  var allOK = true;
+    var pythonScript = __dirname + "/mote.py";
+    var allOK = true;
 
-  try {
-    var cpuinfo = fs.readFileSync("/proc/cpuinfo").toString();
-    if (cpuinfo.indexOf(": BCM") === -1) {
-      RED.log.warn("rpi-mote : " + RED._("node-red:rpi-gpio.errors.ignorenode"));
-      allOK = false;
+    try {
+        var cpuinfo = fs.readFileSync("/proc/cpuinfo").toString();
+        if (cpuinfo.indexOf(": BCM") === -1) {
+            RED.log.warn("rpi-mote : " + RED._("node-red:rpi-gpio.errors.ignorenode"));
+            allOK = false;
+        }
+        else if (!fs.existsSync("/usr/share/doc/python-rpi.gpio")) {
+            RED.log.warn("rpi-mote : " + RED._("node-red:rpi-gpio.errors.libnotfound"));
+            allOK = false;
+        }
+        else if (!fs.existsSync(pythonScript)) {
+            RED.log.warn("rpi-mote : " + RED._("node-red:rpi-gpio.errors.libnotfound"));
+            allOK = false;
+        }
     }
-    else if (!fs.existsSync("/usr/share/doc/python-rpi.gpio")) {
-      RED.log.warn("rpi-mote : " + RED._("node-red:rpi-gpio.errors.libnotfound"));
-      allOK = false;
-    }
-    else if (!fs.existsSync(pythonScript)) {
-      RED.log.warn("rpi-mote : " + RED._("node-red:rpi-gpio.errors.libnotfound"));
-      allOK = false;
-    }
-  }
-  catch (err) {
-    RED.log.warn("rpi-mote : " + RED._("node-red:rpi-gpio.errors.ignorenode"));
-    allOK = false;
-  }
-
-  // the magic to make python print stuff immediately
-  process.env.PYTHONUNBUFFERED = 1;
-
-  function RpiMoteNode(n) {
-    RED.nodes.createNode(this, n);
-    this.brightness = n.brightness || 20;
-    var node = this;
-
-    function inputlistener(msg) {
-      if (typeof msg.payload === "string") {
-        msg.payload = msg.payload.replace('"', '');
-        var s = msg.payload.toUpperCase().split(",");
-
-        if (s.length === 1) {
-          if ((s[0] == "CLS") || (s[0] == "CLR") || (s[0] == "CLEAR")) {
-            //console.log("CLEAR")
-            node.child.stdin.write("C\n");
-          }
-        }
-        else if (s.length === 2) {
-          if (s[0] === "BRIGHTNESS") {
-            //console.log("BRIGHTNESS",s[1])
-            node.child.stdin.write("B" + s[1] + "\n");
-          }
-          if ((s[0] == "CLR") || (s[0] == "CLEAR")) {
-            //console.log("CLEAR CHANNEL",s[1])
-            node.child.stdin.write("C" + s[1] + "\n");
-          }
-        }
-        else if (s.length === 3) {
-          //console.log("FILL",s)
-          node.child.stdin.write('F' + msg.payload + '\n');
-        }
-        else if (s.length % 3 === 0 && s.length <= 48) {
-          node.child.stdin.write('P*,' + msg.payload + '\n');
-        }
-        else if (s.length % 3 === 1 || s.length % 5 === 0 || s.length === 192) {
-          //console.log("PIXELS",s)
-          node.child.stdin.write('P' + msg.payload + '\n');
-        }
-        else if (s.length === 64) {
-          //console.log("BUFFER",s)
-          node.child.stdin.write(msg.payload);
-          node.child.stdin.write("\n");
-        }
-        else { node.warn("Invalid input"); }
-      }
-      else { node.warn("Input not a string"); }
+    catch (err) {
+        RED.log.warn("rpi-mote : " + RED._("node-red:rpi-gpio.errors.ignorenode"));
+        allOK = false;
     }
 
-    if (allOK === true) {
-      //console.log('SPAWNING')
-      node.child = spawn("python", ["-u", pythonScript, node.brightness]);
-      node.status({ fill: "green", shape: "dot", text: "ok" });
+    // the magic to make python print stuff immediately
+    process.env.PYTHONUNBUFFERED = 1;
 
-      node.on("input", inputlistener);
+    function RpiMoteNode(n) {
+        RED.nodes.createNode(this, n);
+        this.brightness = n.brightness || 20;
+        var node = this;
 
-      node.child.stdout.on('data', function (data) {
-        if (RED.settings.verbose) { node.log("out: " + data + " :"); }
-      });
+        function inputlistener(msg) {
+            if (typeof msg.payload === "string") {
+                msg.payload = msg.payload.replace('"', '');
+                var s = msg.payload.toUpperCase().split(",");
 
-      node.child.stderr.on('data', function (data) {
-        if (RED.settings.verbose) { node.log("err: " + data + " :"); }
-      });
-
-      node.child.on('close', function (code, signal) {
-        //console.log('CLOSING: '+ code + ' '+ signal)
-        node.child = null;
-        if (RED.settings.verbose) { node.log(RED._("rpi-gpio.status.closed")); }
-        if (node.done) {
-          node.status({ fill: "grey", shape: "ring", text: "closed" });
-          node.done();
+                if (s.length === 1) {
+                    if ((s[0] == "CLS") || (s[0] == "CLR") || (s[0] == "CLEAR")) {
+                        //console.log("CLEAR")
+                        node.child.stdin.write("C\n");
+                    }
+                }
+                else if (s.length === 2) {
+                    if (s[0] === "BRIGHTNESS") {
+                        //console.log("BRIGHTNESS",s[1])
+                        node.child.stdin.write("B" + s[1] + "\n");
+                    }
+                    if ((s[0] == "CLR") || (s[0] == "CLEAR")) {
+                        //console.log("CLEAR CHANNEL",s[1])
+                        node.child.stdin.write("C" + s[1] + "\n");
+                    }
+                }
+                else if (s.length === 3) {
+                    //console.log("FILL",s)
+                    node.child.stdin.write('F' + msg.payload + '\n');
+                }
+                else if (s.length % 3 === 0 && s.length <= 48) {
+                    //console.log("PIXELS (channel)",s)
+                    node.child.stdin.write('P*,' + msg.payload + '\n');
+                }
+                else if (s.length % 3 === 1 || s.length % 5 === 0 || s.length === 192) {
+                    //console.log("PIXELS (individual)",s)
+                    node.child.stdin.write('P' + msg.payload + '\n');
+                }
+                else if (s.length === 64) {
+                    //console.log("BUFFER",s)
+                    node.child.stdin.write(msg.payload);
+                    node.child.stdin.write("\n");
+                }
+                else { node.warn("Invalid input"); }
+            }
+            else { node.warn("Input not a string"); }
         }
-        else { node.status({ fill: "red", shape: "ring", text: "stopped" }); }
-      });
 
-      node.child.on('error', function (err) {
-        if (err.errno === "ENOENT") { node.error(RED._("rpi-gpio.errors.commandnotfound")); }
-        else if (err.errno === "EACCES") { node.error(RED._("rpi-gpio.errors.commandnotexecutable")); }
-        else { node.error(RED._("rpi-gpio.errors.error") + ': ' + err.errno); }
-      });
+        if (allOK === true) {
+            node.child = spawn("python", ["-u", pythonScript, node.brightness]);
+            node.status({ fill: "green", shape: "dot", text: "ok" });
 
-      node.on("close", function (done) {
-        node.status({ fill: "grey", shape: "ring", text: "closed" });
-        //if (node.tout) { clearTimeout(node.tout); }
-        if (node.child != null) {
-          node.done = done;
-          node.child.kill('SIGKILL');
+            node.on("input", inputlistener);
+
+            node.child.stdout.on('data', function (data) {
+                if (RED.settings.verbose) { node.log("out: " + data + " :"); }
+            });
+
+            node.child.stderr.on('data', function (data) {
+                if (RED.settings.verbose) { node.log("err: " + data + " :"); }
+            });
+
+            node.child.on('close', function (code, signal) {
+                node.child = null;
+                if (RED.settings.verbose) { node.log(RED._("rpi-gpio.status.closed")); }
+                if (node.done) {
+                    node.status({ fill: "grey", shape: "ring", text: "closed" });
+                    node.done();
+                }
+                else { node.status({ fill: "red", shape: "ring", text: "stopped" }); }
+            });
+
+            node.child.on('error', function (err) {
+                if (err.errno === "ENOENT") { node.error(RED._("rpi-gpio.errors.commandnotfound")); }
+                else if (err.errno === "EACCES") { node.error(RED._("rpi-gpio.errors.commandnotexecutable")); }
+                else { node.error(RED._("rpi-gpio.errors.error") + ': ' + err.errno); }
+            });
+
+            node.on("close", function (done) {
+                node.status({ fill: "grey", shape: "ring", text: "closed" });
+                if (node.child != null) {
+                    node.done = done;
+                    node.child.kill('SIGKILL');
+                }
+                else { done(); }
+            });
+
         }
-        else { done(); }
-      });
-      
+        else {
+            node.status({ fill: "grey", shape: "dot", text: "node-red:rpi-gpio.status.not-available" });
+        }
     }
-    else {
-      node.status({ fill: "grey", shape: "dot", text: "node-red:rpi-gpio.status.not-available" });
-    }
-  }
-  RED.nodes.registerType("rpi-mote", RpiMoteNode);
+    RED.nodes.registerType("rpi-mote", RpiMoteNode);
 }
--- hardware/motephat/mote.py
+++ hardware/motephat/mote.py
@@ -9,18 +9,17 @@
 
 # Import library functions we need
 import sys
-import time
 
 try:
-  import motephat
+    import motephat
 except ImportError:
-  print("run: curl https://get.pimoroni.com/motephat | bash\n before trying again.")
-  sys.exit(0)
+    print("run: curl https://get.pimoroni.com/motephat | bash\n before trying again.")
+    sys.exit(0)
 
 try:
-  raw_input          # Python 2
+    raw_input          # Python 2
 except NameError:
-  raw_input = input  # Python 3
+    raw_input = input  # Python 3
 
 motephat.configure_channel(1, 16, False)
 motephat.configure_channel(2, 16, False)
@@ -31,95 +30,95 @@ motephat.set_brightness(float(sys.argv[1])/100)
 
 def parseRange(str, rangeMax):
 
-  # Test for : syntax
-  if ":" in str:
-    return map(int, str.split(":"))
+    # Test for : syntax
+    if ":" in str:
+        return map(int, str.split(":"))
 
-  # Test for wildcard
-  if str == "*":
-    return list(range(rangeMax))
+    # Test for wildcard
+    if str == "*":
+        return list(range(rangeMax))
   
-  # Test for range
-  if "-" in str:
-    i1 = int(str.split('-')[0]) % rangeMax
-    i2 = int(str.split('-')[1]) % rangeMax + 1
-    if i1 >= i2:
-      i2,i1 = i1+1,i2-1
+    # Test for range
+    if "-" in str:
+        i1 = int(str.split('-')[0]) % rangeMax
+        i2 = int(str.split('-')[1]) % rangeMax + 1
+        if i1 >= i2:
+            i2,i1 = i1+1,i2-1
+        return list(range(i1, i2))
+  
+    # Assuming single index
+    i1 = int(str)
+    i2 = int(str)+1
     return list(range(i1, i2))
-  
-  # Assuming single index
-  i1 = int(str)
-  i2 = int(str)+1
-  return list(range(i1, i2))
 
 while True:
-  try:
-    data = raw_input()
-    if len(data) > 0:
-    
-      # Brightness
-      if data[0] == "B":
-        motephat.set_brightness(float(data[1:])/100)
-      
-      # Clear
-      elif data[0] == "C":
-        if len(data) > 1:
-          motephat.clear_channel(int(data[1:]))
-        else:
-          motephat.clear()
+    try:
+        data = raw_input()
+        if len(data) > 0:
+        
+        # Brightness
+        if data[0] == "B":
+            motephat.set_brightness(float(data[1:])/100)
+        
+        # Clear
+        elif data[0] == "C":
+            if len(data) > 1:
+                motephat.clear_channel(int(data[1:]))
+            else:
+                motephat.clear()
 
-      # Fill
-      elif data[0] == 'F':
-        data = data[1:].strip()
-        s = data.split(',')
-        motephat.set_all(int(s[0]), int(s[1]), int(s[2]))
-      
-      # Pixels
-      elif data[0] == "P":
-        data = data[1:].strip()
-        s = data.split(',')
+        # Fill
+        elif data[0] == 'F':
+            data = data[1:].strip()
+            s = data.split(',')
+            motephat.set_all(int(s[0]), int(s[1]), int(s[2]))
+        
+        # Pixels
+        elif data[0] == "P":
+            data = data[1:].strip()
+            s = data.split(',')
 
-        # c syntax
-        if (len(s) % 3 == 1):
-          cRange = parseRange(s[0], 4)
-          m = (len(s)-1)
-          for c in cRange:
-            q = 0
-            for x in range(16):
-              motephat.set_pixel(c+1,x,int(s[q+1]),int(s[q+2]),int(s[q+3]))
-              q += 3
-              if (q >= m):
+            # c syntax
+            if (len(s) % 3 == 1):
+                cRange = parseRange(s[0], 4)
+                m = (len(s)-1)
+                for c in cRange:
+                    q = 0
+                    for x in range(16):
+                        motephat.set_pixel(c+1,x,int(s[q+1]),int(s[q+2]),int(s[q+3]))
+                        q += 3
+                        if (q >= m):
+                            q = 0
+
+            # c + x syntax
+            elif (len(s) % 5 == 0):
+                for p in range(0,len(s),5):
+                    cRange = parseRange(s[p], 4)
+                    xRange = parseRange(s[p+1], 16)
+                    for c in cRange:
+                        for x in xRange:
+                            motephat.set_pixel(c+1,x,int(s[p+2]),int(s[p+3]),int(s[p+4]))
+
+            # full buffer 
+            elif (len(s) == 192):
                 q = 0
+                for p in range(64):
+                    motephat.set_pixel(int(p/16)+1,p%16,data[q],data[q+1],data[q+2])
+                    q += 3
 
-        # c + x syntax
-        elif (len(s) % 5 == 0):
-          for p in range(0,len(s),5):
-            cRange = parseRange(s[p], 4)
-            xRange = parseRange(s[p+1], 16)
-            for c in cRange:
-              for x in xRange:
-                motephat.set_pixel(c+1,x,int(s[p+2]),int(s[p+3]),int(s[p+4]))
+        # Complete pixel buffer
+        else:
+            q = 0
+            for p in range(64):
+                motephat.set_pixel(int(p/16)+1,p%16,ord(data[q]),ord(data[q+1]),ord(data[q+2]))
+                q += 3
+        
+        # Draw an update
+        motephat.show()
 
-        # full buffer 
-        elif (len(s) == 192):
-          q = 0
-          for p in range(64):
-            motephat.set_pixel(int(p/16)+1,p%16,data[q],data[q+1],data[q+2])
-            q += 3
+    except (EOFError, SystemExit):  # hopefully always caused by us sigint'ing the program
+        sys.exit(0)
 
-      # Complete pixel buffer
-      else:
-        q = 0
-        for p in range(64):
-          motephat.set_pixel(int(p/16)+1,p%16,ord(data[q]),ord(data[q+1]),ord(data[q+2]))
-          q += 3
-      
-      # Draw an update
-      motephat.show()
-
-  except (EOFError, SystemExit):  # hopefully always caused by us sigint'ing the program
-    sys.exit(0)
-
-  except Exception as ex:
-    print(ex)
-    print("bad data: "+data)
+    except Exception as ex:
+        print(ex)
+        print("bad data: "+data)
