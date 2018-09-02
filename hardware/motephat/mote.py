4b2709c09db9c521f3037917fd0e5e1351b77ed7
mattbrailsford <me@mattbrailsford.com>
2018-09-02 16:23:58 +0100

    Added channel syntax for pixel fills

--- hardware/motephat/mote.py
+++ hardware/motephat/mote.py
@@ -29,6 +29,29 @@ motephat.configure_channel(4, 16, False)
 
 motephat.set_brightness(float(sys.argv[1])/100)
 
+def parseRange(str, rangeMax):
+
+  # Test for : syntax
+  if ":" in str:
+    return map(int, str.split(":"))
+
+  # Test for wildcard
+  if str == "*":
+    return list(range(rangeMax))
+  
+  # Test for range
+  if "-" in str:
+    i1 = int(str.split('-')[0]) % rangeMax
+    i2 = int(str.split('-')[1]) % rangeMax + 1
+    if i1 >= i2:
+      i2,i1 = i1+1,i2-1
+    return list(range(i1, i2))
+  
+  # Assuming single index
+  i1 = int(str)
+  i2 = int(str)+1
+  return list(range(i1, i2))
+
 while True:
   try:
     data = raw_input()
@@ -55,48 +78,35 @@ while True:
       elif data[0] == "P":
         data = data[1:].strip()
         s = data.split(',')
-        for p in range(0,len(s),5):
 
-          # Parse channel indicies
-          c1 = 0
-          c2 = 0
-          if s[p] == "*":
-            c1 = 0
-            c2 = 4
-          elif "-" in s[p]:
-            c1 = int(s[p].split('-')[0]) % 4
-            c2 = int(s[p].split('-')[1]) % 4 + 1
-            if c1 >= c2:
-              c2,c1 = c1+1,c2-1
-          else:
-            c1 = int(s[p])
-            c2 = int(s[p])+1
-          
-          # print(str(c1) +" "+ str(c2))
+        # c syntax
+        if (len(s) % 3 == 1):
+          cRange = parseRange(s[0], 4)
+          m = (len(s)-1)
+          for c in cRange:
+            q = 0
+            for x in range(16):
+              motephat.set_pixel(c+1,x,int(s[q+1]),int(s[q+2]),int(s[q+3]))
+              q += 3
+              if (q >= m):
+                q = 0
 
-          # Parse pixel indicies
-          x1 = 0
-          x2 = 0
-          if s[p+1] == "*":
-            x1 = 0
-            x2 = 16
-          elif "-" in s[p+1]:
-            x1 = int(s[p+1].split('-')[0]) % 16
-            x2 = int(s[p+1].split('-')[1]) % 16 + 1
-            if x1 >= x2:
-              x2,x1 = x1+1,x2-1
-          else:
-            x1 = int(s[p+1]) % 16
-            x2 = int(s[p+1]) % 16 + 1
-            
-          # print(str(x1) +" "+ str(x2))
+        # c + x syntax
+        elif (len(s) % 5 == 0):
+          for p in range(0,len(s),5):
+            cRange = parseRange(s[p], 4)
+            xRange = parseRange(s[p+1], 16)
+            for c in cRange:
+              for x in xRange:
+                motephat.set_pixel(c+1,x,int(s[p+2]),int(s[p+3]),int(s[p+4]))
 
-          # Draw pixels
-          for c in range(c1,c2):
-            for x in range(x1,x2):
-              # print(str(c+1) +" "+ str(x) +" "+ s[p+2] +" "+ s[p+3] +" "+ s[p+4])
-              motephat.set_pixel(c+1,x,int(s[p+2]),int(s[p+3]),int(s[p+4]))
-      
+        # full buffer 
+        elif (len(s) == 192):
+          q = 0
+          for p in range(64):
+            motephat.set_pixel(int(p/16)+1,p%16,data[q],data[q+1],data[q+2])
+            q += 3
+
       # Complete pixel buffer
       else:
         q = 0
