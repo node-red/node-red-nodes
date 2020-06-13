24bd20ab21d9575c25d00afb02a3320b178aa841
Dave Conway-Jones <conway@uk.ibm.com>
2020-06-13 20:35:14 +0100

    Fix not triggering on waitfor character

--- io/serialport/25-serial.js
+++ io/serialport/25-serial.js
@@ -381,7 +381,7 @@ module.exports = function(RED) {
                                 var c = d[z];
                                 if (c === waitfor) { active = true; }
                                 // handle the trivial case first -- single char buffer
-                                if (!active) { return; }
+                                if (!active) { continue; }
                                 if ((newline === 0)||(newline === "")) {
                                     emitData(new Buffer.from([c]));
                                     continue;
