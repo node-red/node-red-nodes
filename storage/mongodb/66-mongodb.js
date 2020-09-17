c21fb2291897ff6dcf420b7669c285ea0f0aae11
Dave Conway-Jones <conway@uk.ibm.com>
2020-09-17 09:21:50 +0100

    Linting and bump package for new clustered PR.

--- storage/mongodb/66-mongodb.js
+++ storage/mongodb/66-mongodb.js
@@ -21,25 +21,25 @@ module.exports = function(RED) {
 
         var url = "mongodb://";
         if (this.topology === "dnscluster") {
-          url = "mongodb+srv://";
+            url = "mongodb+srv://";
         }
         if (this.credentials && this.credentials.user && this.credentials.password) {
-          this.user = this.credentials.user;
-          this.password =  this.credentials.password;
+            this.user = this.credentials.user;
+            this.password =  this.credentials.password;
         } else {
-          this.user = n.user;
-          this.password = n.password;
+            this.user = n.user;
+            this.password = n.password;
         }
         if (this.user) {
-          url += this.user+":"+this.password+"@";
+            url += this.user+":"+this.password+"@";
         }
         if (clustered) {
-          url += this.hostname + "/" + this.db
+            url += this.hostname + "/" + this.db
         } else {
-          url += this.hostname + ":" + this.port + "/" + this.db;
+            url += this.hostname + ":" + this.port + "/" + this.db;
         }
         if (this.connectOptions){
-          url += "?" + this.connectOptions;
+            url += "?" + this.connectOptions;
         }
 
         console.log("MongoDB URL: " + url);
