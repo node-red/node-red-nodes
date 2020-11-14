b37471440f3bcc1815b376072ec4fa00fcfbab0d
t.kawamorita <takayoshi.kawamorita@gmail.com>
2020-11-15 00:59:28 +0900

    The problem that the input contents disappear when multiple keys are input at the same time.

--- hardware/PiGpio/36-rpi-gpio.js
+++ hardware/PiGpio/36-rpi-gpio.js
@@ -276,11 +276,16 @@ module.exports = function(RED) {
             node.status({fill:"green",shape:"dot",text:"rpi-gpio.status.ok"});
 
             node.child.stdout.on('data', function (data) {
-                var b = data.toString().trim().split(",");
-                var act = "up";
-                if (b[1] === "1") { act = "down"; }
-                if (b[1] === "2") { act = "repeat"; }
-                node.send({ topic:"pi/key", payload:Number(b[0]), action:act });
+                var d = data.toString().trim().split("\n");
+                for (var i = 0; i < d.length; i++) {
+                    if (d[i] !== '') {
+                        var b = d[i].trim().split(",");
+                        var act = "up";
+                        if (b[1] === "1") { act = "down"; }
+                        if (b[1] === "2") { act = "repeat"; }
+                        node.send({ topic:"pi/key", payload:Number(b[0]), action:act });
+                    }
+                }
             });
 
             node.child.stderr.on('data', function (data) {
