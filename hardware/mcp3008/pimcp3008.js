91217d6e62c40c069dff4a7f6bf73c90be29b560
Dave Conway-Jones <conway@uk.ibm.com>
2020-12-01 23:43:48 +0000

    allow mcp node to be visible when not in Pi
    mark as inactive status
    to close #718

--- hardware/mcp3008/pimcp3008.js
+++ hardware/mcp3008/pimcp3008.js
@@ -2,16 +2,23 @@
 module.exports = function(RED) {
     "use strict";
     var fs = require('fs');
+    var allOK = false;
+    var mcpadc;
     // unlikely if not on a Pi
     try {
         var cpuinfo = fs.readFileSync("/proc/cpuinfo").toString();
-        if (cpuinfo.indexOf(": BCM") === -1) { throw "Info : "+RED._("rpi-gpio.errors.ignorenode"); }
+        if (cpuinfo.indexOf(": BCM") === -1) {
+            RED.log.warn("Info : mcp3xxx : Not running on a Pi - Ignoring node");
+        }
+        else {
+            mcpadc = require('mcp-spi-adc');
+            allOK = true;
+        }
     }
     catch(err) {
-        throw "Info : "+RED._("rpi-gpio.errors.ignorenode");
+        RED.log.warn("Info : mcp3xxx : Not running on a Pi - Ignoring node");
     }
 
-    var mcpadc = require('mcp-spi-adc');
     var mcp3xxx = [];
 
     function PiMcpNode(n) {
@@ -26,49 +33,54 @@ module.exports = function(RED) {
         var opt = { speedHz:20000, deviceNumber:node.dnum, busNumber:node.bus };
         var chans = parseInt(this.dev.substr(3));
 
-        try {
-            fs.statSync("/dev/spidev"+node.bus+"."+node.dnum);
-            if (mcp3xxx.length === 0) {
-                for (var i=0; i<chans; i++) {
-                    if (node.dev === "3002") { mcp3xxx.push(mcpadc.openMcp3002(i, opt, cb)); }
-                    if (node.dev === "3004") { mcp3xxx.push(mcpadc.openMcp3004(i, opt, cb)); }
-                    if (node.dev === "3008") { mcp3xxx.push(mcpadc.openMcp3008(i, opt, cb)); }
-                    if (node.dev === "3202") { mcp3xxx.push(mcpadc.openMcp3202(i, opt, cb)); }
-                    if (node.dev === "3204") { mcp3xxx.push(mcpadc.openMcp3204(i, opt, cb)); }
-                    if (node.dev === "3208") { mcp3xxx.push(mcpadc.openMcp3208(i, opt, cb)); }
-                    if (node.dev === "3304") { mcp3xxx.push(mcpadc.openMcp3304(i, opt, cb)); }
+        if (allOK === true) {
+            try {
+                fs.statSync("/dev/spidev"+node.bus+"."+node.dnum);
+                if (mcp3xxx.length === 0) {
+                    for (var i=0; i<chans; i++) {
+                        if (node.dev === "3002") { mcp3xxx.push(mcpadc.openMcp3002(i, opt, cb)); }
+                        if (node.dev === "3004") { mcp3xxx.push(mcpadc.openMcp3004(i, opt, cb)); }
+                        if (node.dev === "3008") { mcp3xxx.push(mcpadc.openMcp3008(i, opt, cb)); }
+                        if (node.dev === "3202") { mcp3xxx.push(mcpadc.openMcp3202(i, opt, cb)); }
+                        if (node.dev === "3204") { mcp3xxx.push(mcpadc.openMcp3204(i, opt, cb)); }
+                        if (node.dev === "3208") { mcp3xxx.push(mcpadc.openMcp3208(i, opt, cb)); }
+                        if (node.dev === "3304") { mcp3xxx.push(mcpadc.openMcp3304(i, opt, cb)); }
+                    }
                 }
+                node.on("input", function(msg) {
+                    var pin = null;
+                    if (node.pin === "M") {
+                        var pay = parseInt(msg.payload.toString());
+                        if ((pay >= 0) && (pay < chans)) { pin = pay; }
+                        else { node.warn("Payload needs to select channel 0 to "+(chans-1)); }
+                    }
+                    else { pin = parseInt(node.pin); }
+                    if (pin !== null) {
+                        mcp3xxx[pin].read(function (err, reading) {
+                            if (err) { node.warn("Read error: "+err); }
+                            else { node.send({payload:reading.rawValue, topic:"adc/"+pin}); }
+                        });
+                    }
+                });
             }
-            node.on("input", function(msg) {
-                var pin = null;
-                if (node.pin === "M") {
-                    var pay = parseInt(msg.payload.toString());
-                    if ((pay >= 0) && (pay < chans)) { pin = pay; }
-                    else { node.warn("Payload needs to select channel 0 to "+(chans-1)); }
+            catch(err) {
+                node.error("Error : Can't find SPI device - is SPI enabled in raspi-config ?");
+            }
+
+            node.on("close", function(done) {
+                if (mcp3xxx.length !== 0) {
+                    var j=0;
+                    for (var i=0; i<chans; i++) {
+                        mcp3xxx[i].close(function() { j += 1; if (j === chans) {done()} });
+                    }
+                    mcp3xxx = [];
                 }
-                else { pin = parseInt(node.pin); }
-                if (pin !== null) {
-                    mcp3xxx[pin].read(function (err, reading) {
-                        if (err) { node.warn("Read error: "+err); }
-                        else { node.send({payload:reading.rawValue, topic:"adc/"+pin}); }
-                    });
-                }
+                else { done(); }
             });
         }
-        catch(err) {
-            node.error("Error : Can't find SPI device - is SPI enabled in raspi-config ?");
+        else {
+            node.status({text:"node inactive."})
         }
-
-        node.on("close", function(done) {
-            if (mcp3xxx.length !== 0) {
-                var j=0;
-                for (var i=0; i<chans; i++) {
-                    mcp3xxx[i].close(function() { j += 1; if (j === chans) {done()} });
-                }
-                mcp3xxx = [];
-            }
-            else { done(); }
-        });
     }
 
     RED.nodes.registerType("pimcp3008",PiMcpNode);
