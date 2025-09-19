9ba401c6c0aba15ba84489e3851929ab4a66c2e5
Gerrit Riessen <gerrit@openmindmap.org>
2025-09-19 18:28:39 +0200

    Update 70-markdown.js

--- parsers/markdown/70-markdown.js
+++ parsers/markdown/70-markdown.js
@@ -20,5 +20,5 @@ module.exports = function(RED) {
             }
         });
     }
-    RED.nodes.registerType("markdown-prop",markdownNode);
+    RED.nodes.registerType("markdown",markdownNode);
 }
