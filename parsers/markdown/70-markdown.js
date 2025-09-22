e9f9b11cf9cc77046d1bba55af2cce04d49619f8
Gerrit Riessen <gerrit@openmindmap.org>
2025-09-22 12:47:23 +0200

    Markdown parser: add property selection (#1109)
    * Update 70-markdown.html
    
    * Update 70-markdown.js
    
    * Update package.json
    
    * Update 70-markdown.html
    
    * Update 70-markdown.js

--- parsers/markdown/70-markdown.js
+++ parsers/markdown/70-markdown.js
@@ -3,14 +3,21 @@ module.exports = function(RED) {
     "use strict";
     var markdownNode = function(n) {
         var md = require('markdown-it')({html:true, linkify:true, typographer:true});
+
         RED.nodes.createNode(this,n);
+
+        this.property = n.property || "payload";
         var node = this;
-        //<div id="nr-markdown" style="font-family:helvetica neue,arial,helvetica,sans-serif; margin:12px">';
+
         node.on("input", function(msg) {
-            if (msg.payload !== undefined && typeof msg.payload === "string") {
-                msg.payload = md.render(msg.payload);
+            var value = RED.util.getMessageProperty(msg, node.property);
+
+            if (value !== undefined && typeof value === "string") {
+                RED.util.setMessageProperty(msg, node.property, md.render(value));
+                node.send(msg);
+            } else {
+                node.warn("No property value found");
             }
-            node.send(msg);
         });
     }
     RED.nodes.registerType("markdown",markdownNode);
