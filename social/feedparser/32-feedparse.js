27d3f40e7251f9643dac8e9476aef9ed1008db50
Dave Conway-Jones <dceejay@gmail.com>
2026-02-16 16:04:29 +0000

    Use try/catch for feedparser async error instead
    to close #1124

--- social/feedparser/32-feedparse.js
+++ social/feedparser/32-feedparse.js
@@ -17,43 +17,46 @@ module.exports = function(RED) {
 
         async function getFeed() {
             if (node.url && typeof node.url === "string" && node.url !== "") {
-                const response = await fetch(node.url).catch((error) => {
-                    node.error("Failed Fetch: "+node.url, error)
-                    node.status({fill:"red",shape:"dot",text:RED._("feedparse.errors.failedfetch")});
-                    return;
-                });
-                if (response.status !== 200) {
-                    node.error("Bad Feed: "+node.url, response)
-                    node.status({fill:"red",shape:"dot",text:response.status+": "+RED._("feedparse.errors.badstatuscode")});
-                    return;
-                }
-                const feed = parseFeed(await response.text());
-                if (node.sendarray === true) {
-                    var msg = JSON.parse(JSON.stringify(feed));
-                    node.send(msg);
-                }
-                else {
-                    for (let a=0; a<feed.items.length; a++) {
-                        const article = feed.items[a];
-                        if (!(article.id in node.seen) || ( node.seen[article.id] !== 0 && node.seen[article.id] != new Date(article.published).getTime())) {
-                            node.seen[article.id] = article.published ? new Date(article.published).getTime() : 0;
-                            const msg = { article: JSON.parse(JSON.stringify(article)) };
-                            msg.topic = msg.article.title;
-                            msg.payload = msg.article.description;
-                            msg.link = msg.article.url
-                            msg.feed = node.url;
+                try {
+                    const response = await fetch(node.url)
+                    if (response.status !== 200) {
+                        node.error("Bad Feed: "+node.url, response)
+                        node.status({fill:"red",shape:"dot",text:response.status+": "+RED._("feedparse.errors.badstatuscode")});
+                        return;
+                    }
+                    const feed = parseFeed(await response.text());
+                    if (node.sendarray === true) {
+                        var msg = JSON.parse(JSON.stringify(feed));
+                        node.send(msg);
+                    }
+                    else {
+                        for (let a=0; a<feed.items.length; a++) {
+                            const article = feed.items[a];
+                            if (!(article.id in node.seen) || ( node.seen[article.id] !== 0 && node.seen[article.id] != new Date(article.published).getTime())) {
+                                node.seen[article.id] = article.published ? new Date(article.published).getTime() : 0;
+                                const msg = { article: JSON.parse(JSON.stringify(article)) };
+                                msg.topic = msg.article.title;
+                                msg.payload = msg.article.description;
+                                msg.link = msg.article.url
+                                msg.feed = node.url;
 
-                            if (node.ignorefirst === true && node.donefirst === false) {
-                                // do nothing
-                            }
-                            else {
-                                node.send(msg);
+                                if (node.ignorefirst === true && node.donefirst === false) {
+                                    // do nothing
+                                }
+                                else {
+                                    node.send(msg);
+                                }
                             }
                         }
                     }
+                    node.status({fill:"green",shape:"dot",text:""});
                 }
-                node.status({fill:"green",shape:"dot",text:""});
+                catch (error) {
+                    node.error("Failed Fetch: "+node.url, error)
+                    node.status({fill:"red",shape:"dot",text:RED._("feedparse.errors.failedfetch")})
+                }
             }
+
             else {
                 node.status({fill:"red",shape:"dot",text:RED._("feedparse.errors.invalidurl")});
                 node.error(RED._("feedparse.errors.invalidurl")+": "+node.url);
